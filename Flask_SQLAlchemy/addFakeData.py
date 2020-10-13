076adcb96b7afa86db51a1c1e59eece406c5a4b1
Juhyoung Lee <juhyoung@MacBook-Pro.local>
2020-10-12 22:32:14 -0400

    fixed database and added connected fake data

--- Flask_SQLAlchemy/addFakeData.py
+++ Flask_SQLAlchemy/addFakeData.py
@@ -10,64 +10,71 @@ from faker import Faker
 fake = Faker()
 
 
-def add_client():
-    letters = string.ascii_lowercase
-    client = Client(
-        email_id=fake.email(),
-        phone_number=random.randint(10000000000, 99999999999),
-        # choose one of the one-hour time zone randomly
-        timezone=random.randint(0, 23),
-        year=random.randint(2021, 2024),  # or can be a bigger range
-        major_minor=''.join(random.choice(letters) for _ in range(30)),
-        classes=''.join(random.choice(letters) for _ in range(50)),
-        partner_request=fake.name(),
-        priorities=fake.sentence(),
-        aim=fake.sentence()
-    )
-    db.session.add(client)
-    db.session.commit()
-
-
-def add_fake_clients():
-    for _ in range(100):
-        add_client()
-
-
-def add_goal(name, email):
-    letters = string.ascii_lowercase
-    goal = Goal(
-        goal_id=''.join(random.choice(letters) for i in range(8)),
-        email_id=email,
-        name=name,
-        progress=0
-    )
-    db.session.add(goal)
-    db.session.commit()
-
-
-def add_fake_goals():
-    db.create_all()
-    for _ in range(100):
-        add_goal(fake.sentence(), fake.email())
-
-
-def add_fake_milestones():
-    letters = string.ascii_lowercase
-    for _ in range(100):
-        milestone = Milestone(
-            Milestone_ID=''.join(random.choice(letters) for i in range(8)),
-            Goal_ID=''.join(random.choice(letters) for i in range(10)),
-            Email_ID=fake.email(),
-            Name=fake.first_name(),
-            Deadline=fake.date_time_this_year(),
-            Date_Completed=fake.date_time_this_year()
+def add_users():
+    for _ in range(10):
+        user = User(
+            email_id=fake.email(),
+            password='passwd',
+            name=fake.name()
         )
-        db.session.add(milestone)
+        db.session.add(user)
+    db.session.commit()
+
+
+def add_clients():
+    users = User.query.all()
+    letters = string.ascii_lowercase
+    for user in users:
+        client = Client(
+            email_id=user.email_id,
+            phone_number=random.randint(10000000000, 99999999999),
+            # choose one of the one-hour time zone randomly
+            timezone=random.randint(0, 23),
+            year=random.randint(2021, 2024),  # or can be a bigger range
+            major_minor=''.join(random.choice(letters) for _ in range(30)),
+            classes=''.join(random.choice(letters) for _ in range(50)),
+            partner_request=fake.name(),
+            priorities=fake.sentence(),
+            aim=fake.sentence()
+        )
+        db.session.add(client)
+    db.session.commit()
+
+
+def add_goals():
+    letters = string.ascii_lowercase
+    clients = Client.query.all()
+    for client in clients:
+        for num in range(random.randint(0, 6)):
+            goal = Goal(
+                goal_id=''.join(random.choice(letters) for i in range(10)),
+                email_id=client.email_id,
+                name=''.join(random.choice(letters) for i in range(10))
+            )
+            db.session.add(goal)
+    db.session.commit()
+
+
+def add_milestones():
+    letters = string.ascii_lowercase
+    clients = Client.query.all()
+    for client in clients:
+        for goal in client.goals:
+            for num in range(random.randint(0, 3)):
+                milestone = Milestone(
+                    Milestone_ID=num,
+                    Goal_ID=goal.goal_id,
+                    Email_ID=client.email_id,
+                    Name=''.join(random.choice(letters) for i in range(8)),
+                    Deadline=fake.date_time_this_year()
+                )
+                db.session.add(milestone)
     db.session.commit()
 
 
 def add_fake_data():
     db.create_all()
-    add_fake_goals()
-    add_fake_milestones()
-    add_fake_clients()
+    add_users()
+    add_clients()
+    add_goals()
+    add_milestones()
--- Flask_SQLAlchemy/models.py
+++ Flask_SQLAlchemy/models.py
@@ -1,53 +1,50 @@
 from sqlalchemy import sql, orm
 from app import db
 from datetime import datetime, timedelta
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class User(db.Model):
     email_id = db.Column(db.String(100), primary_key=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(50))
-    phone_number = db.Column(db.Integer)
-    time_zone = db.Column(db.String(50))
-    year = db.Column(db.Integer)
-    major = db.Column(db.String(50))
-    classes = db.Column(db.String(100))
-    partner_request = db.Column(db.String(50))
-    priorities = db.Column(db.String(50))
-    aim = db.Column(db.String(50))
+    name = db.Column(db.String(100))
     date_created = db.Column(db.DateTime, default=datetime.now)
 
 
 class Client(db.Model):
-    email_id = db.Column(db.String(100), primary_key=True)
+    email_id = db.Column(db.String(100), db.ForeignKey(
+        'user.email_id'), primary_key=True)
     phone_number = db.Column(db.Integer)
     timezone = db.Column(db.Integer)
     year = db.Column(db.Integer)
-    major_minor = db.Column(db.String(200))
-    classes = db.Column(db.String(500))
-    partner_request = db.Column(db.String(50))
+    major_minor = db.Column(db.String(100))
+    classes = db.Column(db.String(100))
+    partner_request = db.Column(db.String(100))
     priorities = db.Column(db.String(500))
     aim = db.Column(db.String(500))
 
+    goals = db.relationship('Goal', backref='client')
+
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
-    # db.ForeignKey('user.email_id') when user table is created
-    # email_id = db.Column(db.Integer, db.ForeignKey(
-    #     'user.email_id'), primary_key=True)
-    email_id = db.Column(db.Integer)
-    name = db.Column(db.String(200), nullable=False)
+    goal_id = db.Column(db.String(100), primary_key=True)
+    email_id = db.Column(db.String(100), db.ForeignKey(
+        'client.email_id'), primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
     date_completed = db.Column(db.DateTime)
-    progress = db.Column(db.Integer, nullable=False)
+    progress = db.Column(db.Integer, default=0)
+
+    milestones = db.relationship('Milestone', backref='goal')
 
 
 class Milestone(db.Model):
-    Milestone_ID = db.Column(db.String(100), primary_key=True)
-    Goal_ID = db.Column(db.String(100), primary_key=True)
-    # Email_ID = db.Column(db.String(100), db.ForeignKey(
-    #     'user.email_id'), primary_key=True)
-    Email_ID = db.Column(db.String(100), primary_key=True)
-    Name = db.Column(db.String(50), nullable=False)
+    Milestone_ID = db.Column(db.Integer, primary_key=True)
+    Goal_ID = db.Column(db.String(100), db.ForeignKey(
+        'goal.goal_id'), primary_key=True)
+    Email_ID = db.Column(db.String(100), db.ForeignKey(
+        'client.email_id'), primary_key=True)
+    Name = db.Column(db.String(100), nullable=False)
     Deadline = db.Column(db.DateTime)
     Date_Completed = db.Column(db.DateTime)
